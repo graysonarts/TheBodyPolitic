22e09e2b62032e970301ee916145d7ab8a5d34c2
Russell Hay <russell.hay@gmail.com>
2020-04-18 22:05:23 -0700

    Swap width and height to project portrait mode

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@ int main( ){
     ofGLWindowSettings settings;
 
     settings.setPosition({0.0f, 0.0f});
-		settings.setSize(1024*scale, 768*scale);
+		settings.setSize(768, 1024);
     settings.windowMode = OF_WINDOW;
 
     auto projectorWin = ofCreateWindow(settings);

8298006cc8ff57f676c07cb5f65ad065047dd1a6
Kai Curtis <kai@substantial.com>
2020-04-20 10:23:29 -0700

    Load data from CSV
    Note that the CSV has been omitted from the repo as I'm not convinced
    (but also not *not* convinced) that we want the data living in there
    (versus the internet, I guess? though it might still be nice to have a
    committed copy of sample data to develop against).
    
    Which is all to say that you'll need a copy of that csv in bin/data, and
    it should be named covid_data.csv. Also note that there's really no
    validation to speak of, so if we end up with poorly formatted data at
    some point, it seems like that could fail more quietly than we'd like.

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,4 +1,5 @@
 #include "ofApp.h"
+#include "covidData.h"
 
 void ofDisplayApp::setup() {
 	ofxGuiEnableHiResDisplay();
@@ -16,41 +17,84 @@ void ofDisplayApp::setup() {
 
 	piMapper.registerFboSource(covid19);
 	piMapper.setup();
+
+	loadCovidCsv();
+
+	ofLog() << "Loaded country . " << covidData[0].countryRegion;
+	ofLog() << "Loaded country . " << covidData[1].countryRegion;
 }
 
-void ofDisplayApp::update() {
-	piMapper.update();
+void ofDisplayApp::loadCovidCsv() {
+	map<string, CovidData::CaseType> caseTypeMap{
+		pair<string, CovidData::CaseType>("Confirmed",
+										  CovidData::CaseType::confirmed),
+		pair<string, CovidData::CaseType>("Deaths",
+										  CovidData::CaseType::deaths)};
+
+	csv.load("covid_data.csv");
+	for (auto x : csv) {
+		CovidData::CaseType caseType;
+
+		if (auto it =
+				caseTypeMap.find(x.getString(CovidData::FieldLabel::Case_Type));
+			it != caseTypeMap.end()) {
+			caseType = it->second;
+		} else {
+			ofLog() << "Couldn't find the thing: "
+					<< x.getString(CovidData::FieldLabel::Case_Type);
+			continue;
+		}
+
+		CovidData tmp = CovidData();
+
+		tmp.caseType = caseType;
+		tmp.cases = x.getInt(CovidData::FieldLabel::Cases);
+		tmp.difference = x.getInt(CovidData::FieldLabel::Difference);
+		tmp.date = x.getString(CovidData::FieldLabel::Date);
+		tmp.countryRegion = x.getString(CovidData::FieldLabel::Country_Region);
+		tmp.provinceState = x.getString(CovidData::FieldLabel::Province_State);
+		tmp.admin2 = x.getString(CovidData::FieldLabel::Admin2);
+		tmp.combinedKey = x.getString(CovidData::FieldLabel::Combined_Key);
+		tmp.fips = x.getInt(CovidData::FieldLabel::FIPS);
+		tmp.latitude = x.getFloat(CovidData::FieldLabel::Lat);
+		tmp.longitude = x.getFloat(CovidData::FieldLabel::Long);
+
+		covidData.push_back(tmp);
+	}
 }
+
+void ofDisplayApp::update() { piMapper.update(); }
 
 void ofDisplayApp::draw() {
 	ofBackground(0.0f);
 	piMapper.draw();
-	if (showGui) gui.draw();
+	if (showGui)
+		gui.draw();
 }
 
-void ofDisplayApp::keyPressed(int key){
+void ofDisplayApp::keyPressed(int key) {
 	if (key == 'i') {
 		showGui = !showGui;
-	} if (key == 'f') {
+	}
+	if (key == 'f') {
 		ofToggleFullscreen();
-	} if (key == ' ') {
+	}
+	if (key == ' ') {
 		drawLine.set(!drawLine.get());
 	}
 	piMapper.keyPressed(key);
 }
 
-void ofDisplayApp::keyReleased(int key){
-	piMapper.keyReleased(key);
-}
+void ofDisplayApp::keyReleased(int key) { piMapper.keyReleased(key); }
 
-void ofDisplayApp::mousePressed(int x, int y, int button){
+void ofDisplayApp::mousePressed(int x, int y, int button) {
 	piMapper.mousePressed(x, y, button);
 }
 
-void ofDisplayApp::mouseReleased(int x, int y, int button){
+void ofDisplayApp::mouseReleased(int x, int y, int button) {
 	piMapper.mouseReleased(x, y, button);
 }
 
-void ofDisplayApp::mouseDragged(int x, int y, int button){
+void ofDisplayApp::mouseDragged(int x, int y, int button) {
 	piMapper.mouseDragged(x, y, button);
 }
--- src/ofApp.h
+++ src/ofApp.h
@@ -1,28 +1,36 @@
 #pragma once
 
+#include "covid19source.h"
+#include "covidData.h"
 #include "ofMain.h"
+#include "ofxCsv.h"
 #include "ofxGui.h"
 #include "ofxPiMapper.h"
-#include "covid19source.h"
+#include <string>
 
 class ofDisplayApp : public ofBaseApp {
 	public:
-		void setup();
-		void update();
-		void draw();
+	void setup();
+	void update();
+	void draw();
 
-		void keyPressed(int key);
-		void keyReleased(int key);
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
+	void keyPressed(int key);
+	void keyReleased(int key);
+	void mouseDragged(int x, int y, int button);
+	void mousePressed(int x, int y, int button);
+	void mouseReleased(int x, int y, int button);
 
 	private:
-		ofxPiMapper piMapper;
-		ofxPanel gui;
-		bool showGui;
-		ofParameter<float> speed;
-		ofParameter<bool> drawLine;
+	ofxPiMapper piMapper;
+	ofxPanel gui;
 
-		Covid19 covid19;
+	bool showGui;
+	ofParameter<float> speed;
+	ofParameter<bool> drawLine;
+
+	Covid19 covid19;
+
+	void loadCovidCsv();
+	ofxCsv csv;
+	std::vector<CovidData> covidData;
 };
